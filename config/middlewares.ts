2970bb2ab9a5a5c00f342cf4715a1cbefd610423
Jan Hasselberg <hasselberg.jan@icloud.com>
2025-01-03 22:38:06 +0100

    🚀 (S3): bootstrap configuration

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,6 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +8,21 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            env("CLOUDFRONT_BASE_URL"),
+          ],
+        },
+      },
+    },
+  },
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,28 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+        config: {
+            provider: "aws-s3",
+            providerOptions: {
+                baseUrl: env("CLOUDFRONT_BASE_URL"),
+                s3Options: {
+                    baseUrl: env("CLOUDFRONT_BASE_URL"),
+                    rootPath: "",
+                    credentials: {
+                        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+                        secretAccessKey: env("AWS_ACCESS_SECRET"),
+                    },
+                    region: env("AWS_REGION"),
+                    params: {
+                        ACL: "private", // <== set ACL to private
+                        Bucket: env("AWS_BUCKET"),
+                    },
+                },
+                actionOptions: {
+                    upload: {},
+                    uploadStream: {},
+                    delete: {},
+                },
+            },
+        },
+    },
+});
